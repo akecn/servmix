8e6f5cd53180ac3e4a82d97da7bd7ab826fbc80f
wuake <ake.wgk@taobao.com>
2015-03-05 15:27:48 +0800

    filo modify support vinyl

--- servfile.js
+++ servfile.js
@@ -1,9 +1,42 @@
 
 var servmix = require('./servmix');
+var through2 = require('through2');
+
+//servmix.fileMode = "vinyl";
 
 servmix.pick('./test', function(stream) {
     stream
-        // .pipe().pipe()
+        .pipe(through2.obj(function(file, enc, callback) {
+            if(file.ext === ".css") {
+                file.ext = ".less";
+
+                require('less').render(file.toString(), {
+                    path: file.url
+                }, function (err, output) {
+                    if (err) {
+                        callback(err);
+                        return;
+                    }
+
+                    file.contents = new Buffer(output.css);
+                    file.ext = ".css";
+
+                    callback(null, file);
+                });
+            }else {
+                callback(null, file);
+            }
+        }))
+        .pipe(through2.obj(function(file, enc, callback) {
+            if(file.ext === ".js") {
+
+                file.contents = "define(function(require, exports, module) {\n{{"+file.toString()+"}}\n});";
+
+                callback(null, file);
+            }else {
+                callback(null, file);
+            }
+        }))
         .pipe(servmix.output());
 });
 
